79306e6e5fcfcb41f392f055073bb43784466317
Anton <anton01connor@gmail.com>
2025-03-01 12:16:19 -0600

    Blank Page

---
+++ apps/v4/app/(app)/blank/page.tsx
@@ -0,0 +1,7 @@
+export default function BlankPage() {
+  return (
+    <div className="flex flex-1 items-center justify-center p-4">
+      <h1 className="text-2xl font-bold">Blank Page</h1>
+    </div>
+  );
+}
